21cbeea31f0c57f29ce49221d91c5de084cf1b5f
NerdyZonky <nerdyzonky@gmail.com>
2015-05-11 16:21:02 +0200

    update 7

--- data/PySync_OSX_updater.sh
+++ data/PySync_OSX_updater.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cd /Applications/PySync && git fetch https://github.com/NerdyZonky/PySync_OSX && git pull https://github.com/NerdyZonky/PySync_OSX
+cd /Applications/PySync_OSX && git fetch https://github.com/NerdyZonky/PySync_OSX && git pull https://github.com/NerdyZonky/PySync_OSX
 
 echo 'Update abgeschlossen'
